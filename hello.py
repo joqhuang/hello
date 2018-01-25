efb79c64107175ff56199f0b98484010191b6bd7
Jackson Huang <questionableinsanity@gmail.com>
2018-01-25 14:19:37 -0500

    fix

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 print("Hello, it's me! :D")
 print("Hello, it's Neil here! :)")
-<<<<<<< HEAD
-print("I was born in February???")
-=======
 print("Hello, it's jon  ＼(￣▽￣)/")
 print("I was born in Feb/ March/ October, Scorpio season...")
->>>>>>> 2cc49bb8b16ea9f3b2f663da0e33c45f5ffea41e
