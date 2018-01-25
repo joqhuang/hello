ccabbaf0cc131547fb7d3feaf70e03fc187f9ca0
neilzhu2 <neilzhu@umich.edu>
2018-01-25 14:28:08 -0500

    n1

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 print("Hello, it's me! :D")
+
 print("Hello, it's Neil here! :)")
+print("I was born in Chongqing.")
+
 print("Hello, it's jon  ＼(￣▽￣)/")
 print("I was born in Feb/ March/ October, Scorpio season...")
 print("one more line?")
